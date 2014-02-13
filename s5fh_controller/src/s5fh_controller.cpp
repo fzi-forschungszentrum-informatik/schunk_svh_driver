e5187c1dd8c746ac6e9b43f156f024a88254e1a8
Lars Pfotzer <pfotzer@fzi.de>
2014-02-13 13:37:41 +0100

    s5fh_controller: Added topics and callback functions for connect, reset and enable.

--- s5fh_controller/src/s5fh_controller.cpp
+++ s5fh_controller/src/s5fh_controller.cpp
@@ -1,6 +1,9 @@
 // ROS includes.
 #include "ros/ros.h"
 
+#include "std_msgs/UInt8.h"
+#include "std_msgs/Empty.h"
+
 #include <driver_s5fh/S5FHFingerManager.h>
 
 /*--------------------------------------------------------------------
@@ -10,32 +13,62 @@
 
 using driver_s5fh::S5FHFingerManager;
 
+// Create pointer to S5FH finger manager object.
+S5FHFingerManager *fm = new S5FHFingerManager;
+
+// Set default serial device name
+std::string serial_device_name = "/dev/ttyUSB1";
+
+void connectCallback(const std_msgs::Empty&)
+{
+  if (fm->isConnected())
+  {
+    fm->disconnect();
+  }
+
+  if (!fm->connect(serial_device_name))
+  {
+    ROS_ERROR("Could not connect to SCHUNK five finger hand with serial device %s", serial_device_name.c_str());
+  }
+}
+
+void resetChannelCallback(const std_msgs::UInt8ConstPtr& channel)
+{
+  fm->resetChannel(static_cast<driver_s5fh::S5FHCHANNEL>(channel->data));
+}
+
+void enableChannelCallback(const std_msgs::UInt8ConstPtr& channel)
+{
+//  fm->enableChannel(static_cast<driver_s5fh::S5FHCHANNEL>(channel->data));
+}
+
 int main(int argc, char **argv)
 {
   // Set up ROS.
   ros::init(argc, argv, "s5fh_controller");
-  ros::NodeHandle n;
+  ros::NodeHandle nh;
 
   icl_core::logging::initialize();
 
-  // set default serial device name
-  std::string serial_device_name = "/dev/ttyUSB0";
+  // Subscribe connect topic (Empty)
+  ros::Subscriber connect_sub = nh.subscribe("connect", 1, connectCallback);
 
-  // Create a new S5FH FingerManager object.
-  S5FHFingerManager *finger_manager = new S5FHFingerManager(serial_device_name);
+  // Subscribe reset channel topic (UInt8)
+  ros::Subscriber reset_sub = nh.subscribe("reset_channel", 1, resetChannelCallback);
 
-  // TODO: Subscribe topics
+  // Subscribe enable channel topic (UInt8)
+  ros::Subscriber enable_sub = nh.subscribe("enable_channel", 1, enableChannelCallback);
 
-  // Tell ROS how fast to run this node. (100 Hz = 10 ms)
-  ros::Rate r(100);
+  // Tell ROS how fast to run this node. (100 = 100 Hz = 10 ms)
+  ros::Rate rate(100);
 
   // Main loop.
-  while (n.ok())
+  while (nh.ok())
   {
-    // TODO: Publish topic.
+    // TODO: Publish joint angle topic.
 
     ros::spinOnce();
-    r.sleep();
+    rate.sleep();
   }
 
   return 0;
